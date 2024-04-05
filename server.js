bb61a8b773fbdcf05e70c35f16cbbc0651911c4b
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-04-05 23:46:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors= require('cors');
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -12,7 +10,7 @@ const categoryDataAddedToDBRouter = require("./routes/categoryimport.router");
 
 const hotelRouter = require("./routes/hotel.router");
 const categoryRouter = require("./routes/category.router");
-const singleHotelRouter = require("./routes/singlehotel.router");
+const singleHoterRouter = require("./routes/singlehotel.router");
 const authRouter = require("./routes/auth.router");
 const wishlistRouter = require("./routes/wishlist.router");
 
@@ -27,8 +25,8 @@ connectDB();
 const PORT = 3500;
 
 app.get("/", (req, res) => {
-    res.send("Hello Geeks");
-})
+  res.send("Hello Geeks");
+});
 
  app.listen(PORT, () => {
      console.log(`Travel listening on port ${PORT}`)
@@ -38,14 +36,13 @@ app.use("/api/hoteldata", hotelDataAddedToDBRouter);
 app.use("/api/categorydata", categoryDataAddedToDBRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/category", categoryRouter);
-app.use("/api/hotels", singleHotelRouter);
+app.use("/api/hotels", singleHoterRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/wishlist", wishlistRouter);
 
-
 mongoose.connection.once("open", () => {
-    console.log("Connected to DB");
-    app.listen(process.env.PORT || PORT, () => {
-        console.log("Server is Up and Running");
-    })
+  console.log("Connected to DB");
+  app.listen(process.env.PORT || PORT, () => {
+    console.log("Server is Up and Running");
+  });
 });
