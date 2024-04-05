3c00a1a0af98f316d2e948133b1e918b3cd7712f
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-04-05 23:14:06 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+import cors from 'cors';
+
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors= require('cors');
+const corsOptions = {
+   origin: 'http://localhost:3000',
+   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   credentials: true,
+   optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 dotenv.config();
 
